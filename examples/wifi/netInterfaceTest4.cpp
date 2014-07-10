871c895c7a1201dfed64d35408498f3ae9d75b77
Alessio Balsini <a.balsini@sssup.it>
2014-07-10 19:40:20 +0200

    Completed forwarding test

--- examples/wifi/netInterfaceTest4.cpp
+++ examples/wifi/netInterfaceTest4.cpp
@@ -27,10 +27,9 @@ TEST_CASE( "netInterface Test, forwarding verification", "[netInterfaceForwardin
    * has to be sent to N and then forwarded to D.
    */
 
-  /*
-  auto interfacesTrace = std::make_shared<TraceAscii>("netInterfacesTrace3.txt");
+  auto interfacesTrace = std::make_shared<TraceAscii>("netInterfacesTrace4.txt");
 
-  std::vector<double> times{1, 1000, 2000};
+  std::vector<double> times{10};
 
   auto at = std::make_shared<DetVar>(times);
 
@@ -39,26 +38,35 @@ TEST_CASE( "netInterface Test, forwarding verification", "[netInterfaceForwardin
                                     at,
                                     1);
   auto ss = static_pointer_cast<Node>(s);
-  auto d = std::make_shared<Node>(std::string("D"),
+  auto n = std::make_shared<Node>(std::string("N"),
                                   std::make_pair(0, 1));
+  auto d = std::make_shared<Node>(std::string("D"),
+                                  std::make_pair(0, 2));
 
   auto s_int = std::make_shared<WifiInterface>("S_int", 1.1, ss);
+  auto n_int = std::make_shared<WifiInterface>("N_int", 1.1, n);
   auto d_int = std::make_shared<WifiInterface>("D_int", 1.1, d);
 
   s_int->addTrace(interfacesTrace);
+  n_int->addTrace(interfacesTrace);
   d_int->addTrace(interfacesTrace);
 
   auto s_lnk = std::make_shared<WifiLink>("S_lnk");
   auto d_lnk = std::make_shared<WifiLink>("D_lnk");
+  auto n_lnk = std::make_shared<WifiLink>("N_lnk");
 
   s_int->link(s_lnk);
   d_int->link(d_lnk);
+  n_int->link(n_lnk);
 
-  s_lnk->addInterface(d_int);
-  d_lnk->addInterface(s_int);
+  s_lnk->addInterface(n_int);
+  d_lnk->addInterface(n_int);
+  n_lnk->addInterface(s_int);
+  n_lnk->addInterface(d_int);
 
   s->netInterface(s_int);
   d->netInterface(d_int);
+  n->netInterface(n_int);
 
   s->addDest(d);
 
@@ -66,51 +74,128 @@ TEST_CASE( "netInterface Test, forwarding verification", "[netInterfaceForwardin
   {
     SIMUL.initSingleRun();
 
-    REQUIRE( d_int->status() == IDLE );
     REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == IDLE );
+    REQUIRE( d_int->status() == IDLE );
 
+    std::cout << std::endl;
     SIMUL.sim_step();
 
     REQUIRE( s_int->status() == WAITING_FOR_DIFS );
+    REQUIRE( n_int->status() == IDLE );
     REQUIRE( d_int->status() == IDLE );
 
+    std::cout << std::endl;
     SIMUL.sim_step();
 
     REQUIRE( s_int->status() == SENDING_MESSAGE );
-    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
-
-    SIMUL.sim_step();
-
-    REQUIRE( s_int->status() == WAITING_FOR_ACK );
-    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
-
-    SIMUL.sim_step();
-
-    REQUIRE( s_int->status() == WAITING_FOR_ACK );
-    REQUIRE( d_int->status() == WAITING_FOR_SIFS );
-
-    SIMUL.sim_step();
-
-    REQUIRE( s_int->status() == RECEIVING_MESSAGE );
-    REQUIRE( d_int->status() == SENDING_ACK );
-
-    SIMUL.sim_step();
-
-    REQUIRE( s_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( n_int->status() == RECEIVING_MESSAGE );
     REQUIRE( d_int->status() == IDLE );
 
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == WAITING_FOR_ACK );
+    REQUIRE( n_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( d_int->status() == IDLE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == WAITING_FOR_ACK );
+    REQUIRE( n_int->status() == WAITING_FOR_SIFS );
+    REQUIRE( d_int->status() == IDLE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( n_int->status() == SENDING_ACK );
+    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( n_int->status() == WAITING_FOR_DIFS );
+    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
+
+    std::cout << std::endl;
     SIMUL.sim_step();
 
     REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == WAITING_FOR_DIFS );
+    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == WAITING_FOR_DIFS );
     REQUIRE( d_int->status() == IDLE );
 
-    // No more events in queue
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( n_int->status() == SENDING_MESSAGE );
+    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( n_int->status() == WAITING_FOR_ACK );
+    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == WAITING_FOR_ACK );
+    REQUIRE( d_int->status() == RECEIVING_MESSAGE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == WAITING_FOR_ACK );
+    REQUIRE( d_int->status() == WAITING_FOR_SIFS );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( d_int->status() == SENDING_ACK );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == RECEIVING_MESSAGE );
+    REQUIRE( d_int->status() == IDLE );
+
+    std::cout << std::endl;
+    SIMUL.sim_step();
+
+    REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == IDLE );
+    REQUIRE( d_int->status() == IDLE );
+
+    SIMUL.run_to(10000);
+
+    REQUIRE( s_int->status() == IDLE );
+    REQUIRE( n_int->status() == IDLE );
+    REQUIRE( d_int->status() == IDLE );
 
     REQUIRE_THROWS( SIMUL.getNextEventTime() );
+
+    // No more events in queue
 
     SIMUL.endSingleRun();
   }
 
   interfacesTrace->close();
-  */
+
 }
